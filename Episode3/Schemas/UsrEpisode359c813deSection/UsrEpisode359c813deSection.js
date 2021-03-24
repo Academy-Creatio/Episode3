1ee7f1edb6679feddd30babb761f69b86b93a572
Kyryl Krylov, CPA <k.krylov@creatio.com>
2021-03-24 15:55:13 +0200

    Added New Section

---
+++ Episode3/Schemas/UsrEpisode359c813deSection/UsrEpisode359c813deSection.js
@@ -0,0 +1,8 @@
+define("UsrEpisode359c813deSection", [], function() {
+	return {
+		entitySchemaName: "UsrEpisode3",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
